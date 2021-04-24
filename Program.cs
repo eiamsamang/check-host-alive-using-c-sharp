2b0545fbefda7d0cba38356d361dbc28d8c4b18d
Pitak <pita.e@outlook.com>
2021-04-24 08:12:32 +0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection.Emit;
+using System.Net.NetworkInformation;
+using System;
+
+namespace TestHostAlive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testOper = TestHostAlive("www.google.com");
+            if (testOper == true)
+            {
+                Console.WriteLine($"www.google.com is alive.");
+            }
+            else
+            {
+                Console.WriteLine($"www.google.com is down!");
+            }
+            //Console.WriteLine(testOper);
+        }
+
+        private static bool TestHostAlive(string server)
+        {
+            var hostIsAlive = false;
+            try
+            {
+                var ping = new Ping();
+                var pingreply = ping.Send(server, 2000);
+                if (pingreply.Status == IPStatus.Success)
+                {
+                    hostIsAlive = true;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                hostIsAlive = false;
+            }
+            return (hostIsAlive);
+        }
+    }
+}
